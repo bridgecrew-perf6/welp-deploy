4a9d2dfbe9a2b18b26aa123092eb12d412faa66d
Sonny Li <sonnynomnom@gmail.com>
2022-05-02 15:05:56 -0400

    update

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const path = require('path');
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGO_URL, { 
+    useNewUrlParser: true ,
+    // useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected!"))
+  .catch(err => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Backend server is running!");
+});
