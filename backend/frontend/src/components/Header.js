f70289407adeb2db39ae73c2ce242bdf2c4eb4e7
Sonny Li <sonnynomnom@gmail.com>
2022-05-24 04:40:59 -0400

    Update Header.js

--- backend/frontend/src/components/Header.js
+++ backend/frontend/src/components/Header.js
@@ -18,7 +18,7 @@ import MoreIcon from '@mui/icons-material/MoreVert';
 import "./header.css";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import TikTokIcon from './tiktok.svg';
+import TikTokIcon from '../../public/tiktok.svg';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
